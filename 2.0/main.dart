ec5ee1c4339542888591662f028764aae5dd5a8a
leeyeji0306 <s2548@e-mirim.hs.kr>
2026-04-09 18:51:12 +0900

    2.1

---
+++ 2.0/main.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = 'nico';
+  bool alive = true;
+  int age = 12;
+  double money = 69.99;
+  //dart는 거의 대부분이 객체로 되어있음. 심지어 function도 객체로 되어있어서 진정한 객체 지향 언어임.
+  num x = 12; //int와 double은 num에 상속을 받아서 만들어짐.
+  x = 1.1; //그래서 실수일 수도 있음.
+}
