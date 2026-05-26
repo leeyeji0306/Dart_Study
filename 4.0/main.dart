6415509ade501aa9f0706dc0e20b5b031e8bfd91
leeyeji0306 <s2548@e-mirim.hs.kr>
2026-05-27 00:21:28 +0900

    4.3

--- 4.0/main.dart
+++ 4.0/main.dart
@@ -1,8 +1,7 @@
 class Player {
   final String name;
-  int xp;
+  int xp, age;
   String team;
-  int age;
 
   Player({
     required this.name,
@@ -10,6 +9,23 @@ class Player {
     required this.team,
     required this.age,
   }); //이거 생성자임 ㄷㄷ, 파라미터 위치 중요함.
+
+  Player.createBluePlayer({
+    required String name,
+    required int age,
+  }) //Player 초기화, 생성자가 아니라 메소드임.
+  : this.age = age,
+       this.name = name,
+       this.team = 'blue',
+       this.xp = 0;
+
+  Player.createRedPlayer(String name, int age)
+    : this.age = age,
+      this.name = name,
+      this.team = 'red',
+      this.xp = 0;
+
+  //:(콜론) : 사용자에게 받은 값들로 Pleayer 객체를 초기화하겠다고 알려주는 기호.
 
   void sayHello() {
     print("Hi my name is $name");
@@ -20,15 +36,13 @@ class Player {
 //변수를 사용할 때 this.을 사용하지 않음.
 
 void main() {
-  var player = Player(
+  var player = Player.createBluePlayer(
     name: 'nico',
-    xp: 1500,
-    team: 'red',
     age: 21,
   ); //객체를 만들 땐 new 연산자를 쓰지 않음.
   player.sayHello();
-  var player2 = Player(name: 'lynn', xp: 2500, team: 'blue', age: 12);
-  player2.sayHello();
+  var redPlayer = Player.createRedPlayer('lynn', 12);
+  redPlayer.sayHello();
 }
 
 //dart는 거의 다 class로 이루어져 있음. 매우매우 중요함.
