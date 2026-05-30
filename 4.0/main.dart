5ede218d6bf1a9a4dc366a1674457b95b92c812e
leeyeji0306 <s2548@e-mirim.hs.kr>
2026-05-30 23:04:38 +0900

    4.5

--- 4.0/main.dart
+++ 4.0/main.dart
@@ -1,29 +1,34 @@
 class Player {
   final String name;
-  int xp, age;
+  int xp;
   String team;
 
-  Player({
-    required this.name,
-    required this.xp,
-    required this.team,
-    required this.age,
-  }); //이거 생성자임 ㄷㄷ, 파라미터 위치 중요함.
+  Player.fromJson(Map<String, dynamic> playerJson)
+    : name = playerJson['name'],
+      xp = playerJson['xp'],
+      team = playerJson['team'];
 
-  Player.createBluePlayer({
-    required String name,
-    required int age,
-  }) //Player 초기화, 생성자가 아니라 메소드임.
-  : this.age = age,
-       this.name = name,
-       this.team = 'blue',
-       this.xp = 0;
+  // Player({
+  //   required this.name,
+  //   required this.xp,
+  //   required this.team,
+  //   required this.age,
+  // }); //이거 생성자임 ㄷㄷ, 파라미터 위치 중요함.
 
-  Player.createRedPlayer(String name, int age)
-    : this.age = age,
-      this.name = name,
-      this.team = 'red',
-      this.xp = 0;
+  // Player.createBluePlayer({
+  //   required String name,
+  //   required int age,
+  // }) //Player 초기화, 생성자가 아니라 메소드임.
+  // : this.age = age,
+  //      this.name = name,
+  //      this.team = 'blue',
+  //      this.xp = 0;
+
+  // Player.createRedPlayer(String name, int age)
+  //   : this.age = age,
+  //     this.name = name,
+  //     this.team = 'red',
+  //     this.xp = 0;
 
   //:(콜론) : 사용자에게 받은 값들로 Pleayer 객체를 초기화하겠다고 알려주는 기호.
 
@@ -36,13 +41,23 @@ class Player {
 //변수를 사용할 때 this.을 사용하지 않음.
 
 void main() {
-  var player = Player.createBluePlayer(
-    name: 'nico',
-    age: 21,
-  ); //객체를 만들 땐 new 연산자를 쓰지 않음.
-  player.sayHello();
-  var redPlayer = Player.createRedPlayer('lynn', 12);
-  redPlayer.sayHello();
+  // var player = Player.createBluePlayer(
+  //   name: 'nico',
+  //   age: 21,
+  // ); //객체를 만들 땐 new 연산자를 쓰지 않음.
+  // player.sayHello();
+  // var redPlayer = Player.createRedPlayer('lynn', 12);
+  // redPlayer.sayHello();
+  var apiData = [
+    {"name": "nico", "team": "red", "xp": 0},
+    {"name": "lynn", "team": "red", "xp": 0},
+    {"name": "dal", "team": "red", "xp": 0},
+  ];
+
+  apiData.forEach((playerJson) {
+    var player = Player.fromJson(playerJson);
+    player.sayHello();
+  });
 }
 
 //dart는 거의 다 class로 이루어져 있음. 매우매우 중요함.
